45d4555985d84761c45b2801294d08c76e68d62c
Nick <efutin.kolya@yandex.ru>
2022-09-22 22:44:09 +0300

    done

---
+++ Program15.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Для информции являеется ли день выходным введите номер дня недели.");
+int input = Convert.ToInt32(Console.ReadLine());
+    if (input>5 && input<8)
+        Console.WriteLine("да, это выходной");
+    else if (input<6)
+        Console.WriteLine("нет, это будний день");
+    else if (input>7)
+        Console.WriteLine("Нет такого дня недели");
